80c8fd8e7057f55f22a8dd4fc68d9888c2b4eb86
ahmad jamal karzoun <Ahmadkarzun94@gmail.com>
2023-10-26 21:37:15 +0300

    add currying

---
+++ currying.dart
@@ -0,0 +1,18 @@
+
+void main() {
+  int sum (int n1,int n2){
+    return n1+n2;
+  }
+int Function(int) sumCurrying(int number1){
+  return(int number2){
+    return sum(number1,number2);
+  };
+}
+final numb1=sumCurrying(5); 
+final numb2=numb1(20);
+ 
+ print(numb2);
+
+
+  
+}
